061baa84684342c4783b09e014c0b49d3438de2b
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2023-07-24 15:47:57 -0300

    config para funcionamento do typescript

---
+++ server/build/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+// no app.get('/', () => {}) => estou informando a ROTA e o que ela irá FAZER quando essa rota for acessada
+// request => aquilo que irei "pegar" da página
+// response => a resposta que enviarei
+app.get('/ads', (request, response) => {
+    return response.json([
+        { id: 1, name: 'Anúncio 1' },
+        { id: 2, name: 'Anúncio 2' },
+        { id: 3, name: 'Anúncio 3' },
+    ]);
+});
+// defino qual porta o app será rodado
+app.listen(3333);
