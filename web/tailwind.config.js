96bb233becf2cd4fd2deb40628552b0927806831
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2023-07-25 22:25:21 -0300

    init tailwind for web

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -8,6 +8,7 @@ export default {
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(83deg, #9572FC 0%, #43E7AD 50.52%, #E1D55D 100%)',
       },
     },
   },
