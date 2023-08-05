6f84eb6733c01ec19c99f8609325929c8e4ebc54
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2023-08-05 10:56:49 -0300

    layout web finished

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,10 +5,14 @@ export default {
     './index.html',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient': 'linear-gradient(83deg, #9572FC 0%, #43E7AD 50.52%, #E1D55D 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.90) 67.08%)',
       },
     },
   },
