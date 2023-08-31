0c635e0ad62b38686ecdb11616e7c9eb2a195c32
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2023-08-31 05:54:19 -0300

    logo and headind component

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
 
 import { styles } from './styles';
+import { Heading } from '../../components/Heading';
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image 
+        source={logoImg}
+        style={styles.logo}
+      />
 
+      <Heading 
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
     </View>
   );
 }
